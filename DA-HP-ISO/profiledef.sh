5aba345093a06c61b819216276e7644d833f4b59
DaRipper91 <daripper91@gmail.com>
2026-06-10 10:11:23 -0500

    fix(ci): complete UEFI profiledef and add linux-zen to packages

--- DA-HP-ISO/profiledef.sh
+++ DA-HP-ISO/profiledef.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
 iso_name="aura-logic-hub"
 iso_label="AURA_LOGIC"
 iso_publisher="Aura // Gema"
@@ -5,10 +8,15 @@ iso_application="Headless AI Orchestrator"
 iso_version="1.0"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp' 'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
+# Pure UEFI / systemd-boot
+bootmodes=(
+    'uefi-x64.systemd-boot.esp'
+    'uefi-x64.systemd-boot.eltorito'
+)
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_permissions=(
+
+file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/usr/local/bin/aura-init.sh"]="0:0:755"
